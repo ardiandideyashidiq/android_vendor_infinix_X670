9988e6d37c940d1149e1dd63917dd417cea77ead
Ardiandi Deya Shidiq <rdndds@gmail.com>
2026-08-02 11:57:29 +0000

    update kaeru

--- Android.mk
+++ Android.mk
@@ -7,7 +7,7 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),X670)
 
 $(call add-radio-file-sha1-checked,radio/gz.img,77b38225c4aa095805a6f72b704ca13fffe2c4df)
-$(call add-radio-file-sha1-checked,radio/lk.img,a94fb14bd5009c6b8d590100eead8041839f620f)
+$(call add-radio-file-sha1-checked,radio/lk.img,2005c0d5a1bb76a6bb6b7b2f84588abfa5528bf3)
 $(call add-radio-file-sha1-checked,radio/logo.img,2a665a876fc4e39c97ae1e7c45be49dab9ca4ca5)
 $(call add-radio-file-sha1-checked,radio/md1img.img,5611d89ccac499b927082ce9166d3b46ca57e3a9)
 $(call add-radio-file-sha1-checked,radio/pi_img.img,488f4f4928dab2cb2f974a1bbf9154f5eaddaaeb)
