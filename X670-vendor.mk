68007035a4471dafd4616f15905e36f605ed32ac
rdndds <rdndds@gmail.com>
2026-08-29 22:23:17 +0700

    audio call

--- X670-vendor.mk
+++ X670-vendor.mk
@@ -491,6 +491,10 @@ PRODUCT_PACKAGES += \
     vendor.mediatek.hardware.audio@7.1 \
     vendor.mediatek.hardware.keymaster_attestation@1.0 \
     vendor.mediatek.hardware.keymaster_attestation@1.1 \
+    android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio@6.0-impl-mediatek \
+    android.hardware.audio@7.0-impl-mediatek \
     awinic.audio.effect \
     libHEVCdec_sa.ca7.android \
     libfgauge_gm30 \
